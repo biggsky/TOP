6f249bec0ace96815e3fe9138744709d715faefd
biggsky <sunghyun0624@naver.com>
2023-12-18 13:36:14 +0900

    1218

--- front/src/util/util.js
+++ front/src/util/util.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const ipUrl = axios.create({
   // baseURL: "http://localhost:8080", // 로컬
-  // baseURL: "https://www.hyunss.com", // aws
-  baseURL: "https://www.hyunss.com/api", // 될거같은데..
+  baseURL: "https://www.hyunss.com", // aws
+  // baseURL: "https://www.hyunss.com", // 될거같은데..
   withCredentials: true,
 });
 export { ipUrl };
